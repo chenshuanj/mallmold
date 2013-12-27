f9b47c8e719d987d827a0d83c347a0b83e8ffc7f
betashepherd <betashepherd@gmail.com>
2013-12-27 17:50:34 +0800

    add update payment sql file

---
+++ lib/alipay/update_payment.sql
@@ -0,0 +1,5 @@
+INSERT INTO `mallmold`.`mm_payment` VALUES (NULL,'Alipay Direct','支付宝及时到帐','alipay_direct',6,0,1);
+INSERT INTO `mallmold`.`mm_payment` VALUES (NULL,'Alipay','支付宝','alipay',7,0,1);
+INSERT INTO `mallmold`.`mm_payment` VALUES (NULL,'Alipay Escow','支付宝担保交易','alipay_escow',8,0,1);
+INSERT INTO `mallmold`.`mm_payment` VALUES (NULL,'Alipay Bankpay','支付宝网银支付','alipay_bankpay',9,0,1);
+INSERT INTO `mallmold`.`mm_payment` VALUES (NULL,'Tenpay','财付通','tenpay',9,0,1);
