b13b38afaadb757818a2ff417ff2d129873b9126
betashepherd <betashepherd@gmail.com>
2014-01-03 23:23:47 +0800

    remove comment

--- admin/model/mdata.php
+++ admin/model/mdata.php
@@ -1,18 +1,5 @@
 <?php
-/*
-*	@mdata.php
-*	Copyright (c)2013 Mallmold Ecommerce(HK) Limited. 
-*	http://www.mallmold.com/
-*	
-*	This program is free software; you can redistribute it and/or
-*	modify it under the terms of the GNU General Public License
-*	as published by the Free Software Foundation; either version 2
-*	of the License, or (at your option) any later version.
-*	More details please see: http://www.gnu.org/licenses/gpl.html
-*	
-*	If you want to get an unlimited version of the program or want to obtain
-*	additional services, please send an email to <service@mallmold.com>.
-*/
+
 
 class mdata extends model
 {
--- app/action/pageAction.php
+++ app/action/pageAction.php
@@ -1,18 +1,5 @@
 <?php
-/*
-*	@pageAction.php
-*	Copyright (c)2013 Mallmold Ecommerce(HK) Limited. 
-*	http://www.mallmold.com/
-*	
-*	This program is free software; you can redistribute it and/or
-*	modify it under the terms of the GNU General Public License
-*	as published by the Free Software Foundation; either version 2
-*	of the License, or (at your option) any later version.
-*	More details please see: http://www.gnu.org/licenses/gpl.html
-*	
-*	If you want to get an unlimited version of the program or want to obtain
-*	additional services, please send an email to <service@mallmold.com>.
-*/
+
 
 require Action('common');
 
@@ -44,12 +31,7 @@ class pageAction extends commonAction
 		$this->view('page/index.html');
 	}
 	
-	/*
-	{tag type="goods/list" where="***" limit="*" order="time" tpl="tag/goods_list.html"}
-	{tag type="article/list" cate_id="*" limit="*" tpl="tag/article_list.html"}
-	{tag type="slider" id="***" tpl="tag/slider.html"}
-	{tag type="block" id="***"}
-	*/
+	
 	private function parse($str)
 	{
 		$str = preg_replace("/\{tag\s+(.+?)\}/ies", "\$this->build('\\1');", $str);
